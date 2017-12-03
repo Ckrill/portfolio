672ac82749ef5cd890b82fd2cbdd8c9ba7b05442
Chris Toft Christensen <chris.christensen@valtech.com>
2017-12-03 13:01:46 +0100

    Reworked resume list to the BEM naming convention

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 // JS lint
 
 /*jslint browser: true*/
-/*global alert, func, document, window, setTimeout*/
+/*global document, window, setTimeout*/
 /*jslint plusplus: true */
 
 // JS lint - END
@@ -11,46 +11,46 @@
 // Require dependencies for this module
 
 var isCollection = function (el) {
-    if (el === undefined) return false;
+		if (el === undefined) return false;
 
-    var protoName = el.toString();
+		var protoName = el.toString();
 
-    return (
-        protoName.indexOf("HTMLCollection") != -1 ||
-        protoName.indexOf("NodeList") != -1 ||
-        Array.isArray(el)
-    );
-},
-callEach = function (el, callback) {
-	if (isCollection(el)) {
-		functions.forEach(el, callback);
-	} else {
-		callback(el);
+		return (
+			protoName.indexOf("HTMLCollection") != -1 ||
+			protoName.indexOf("NodeList") != -1 ||
+			Array.isArray(el)
+		);
+	},
+	callEach = function (el, callback) {
+		if (isCollection(el)) {
+			functions.forEach(el, callback);
+		} else {
+			callback(el);
+		}
 	}
-}
 
 var functions = {
-	
-    addEventListener: function (el, eventNames, callback, parent) {
-        if (parent) {
-            callEach(parent, function (e) {
-                eventNames.split(" ").forEach(function (eventName) {
-                    e.addEventListener(eventName, callback);
-                });
-            });
-        } else {
-            callEach(el, function (e) {
-                eventNames.split(" ").forEach(function (eventName) {
-                    e.addEventListener(eventName, callback);
-                });
-            });
-        }
+
+	addEventListener: function (el, eventNames, callback, parent) {
+		if (parent) {
+			callEach(parent, function (e) {
+				eventNames.split(" ").forEach(function (eventName) {
+					e.addEventListener(eventName, callback);
+				});
+			});
+		} else {
+			callEach(el, function (e) {
+				eventNames.split(" ").forEach(function (eventName) {
+					e.addEventListener(eventName, callback);
+				});
+			});
+		}
 	},
-	
+
 	forEach: function (elements, callback) {
 		Array.prototype.forEach.call(elements, callback);
 	},
-	
+
 	hasClass: function (el, className) {
 		if (isCollection(el)) {
 			el = el[0];
@@ -58,39 +58,39 @@ var functions = {
 
 		return el.classList.contains(className);
 	},
-	
+
 	setStyle: function (el, property, styleValue) {
 		callEach(el, function (e) {
 			e.style[property] = styleValue;
 		});
 	},
-	
+
 	select: function (selector, parent) {
-        if (isCollection(parent)) {
-            parent = parent[0];
-        }
+		if (isCollection(parent)) {
+			parent = parent[0];
+		}
 
-        if (!parent) {
-            parent = document;
-        }
+		if (!parent) {
+			parent = document;
+		}
 
-        if (selector.indexOf(" ") === -1 && selector.indexOf(":") === -1) {
-            if (selector.lastIndexOf(".") === 0) {
-                return parent.getElementsByClassName(selector.substring(1));
-            }
+		if (selector.indexOf(" ") === -1 && selector.indexOf(":") === -1) {
+			if (selector.lastIndexOf(".") === 0) {
+				return parent.getElementsByClassName(selector.substring(1));
+			}
 
-            if (selector.lastIndexOf("#") === 0) {
-                return parent.getElementById(selector.substring(1));
-            }
-        }
+			if (selector.lastIndexOf("#") === 0) {
+				return parent.getElementById(selector.substring(1));
+			}
+		}
 
-        return parent.querySelectorAll(selector);
+		return parent.querySelectorAll(selector);
 	},
-	
-    toggleClass: function (el, className) {
-        callEach(el, function (e) {
-            e.classList.toggle(className);
-        });
+
+	toggleClass: function (el, className) {
+		callEach(el, function (e) {
+			e.classList.toggle(className);
+		});
 	}
 }
 
@@ -101,28 +101,29 @@ var f = functions;
 
 // Settings
 
-var numberOfReferences = f.select(".references .page").length, // Defining number of references
-
-    pageWidth = 0.8; // Both for .portfolio and .references
-//    pageMargin = (1 - pageWidth) / 2; // Both for .portfolio and .references
+var settings = {
+	numberOfReferences: f.select(".references .page").length, // Defining number of references
+	pageWidth: 0.8 // Both for .portfolio and .references
+//    pageMargin: (1 - pageWidth) / 2; // Both for .portfolio and .references
+}
 
 // Settings - END
 
 
 
-function listShowLowPriority() {
-	f.toggleClass(f.select('.experience'), 'priorityLowVisible');
-			
+function listShowLowPriority(list, toggleItem) {
+	f.toggleClass(list, 'list--show-all');
+
 	setTimeout(function () {
-		f.toggleClass(f.select('.priorityLow'), 'hidden');
-	}, 100);
+		f.toggleClass(toggleItem, 'hidden');
+	}, 150);
 }
 
-function listHideLowPriority() {
-	f.toggleClass(f.select('.priorityLow'), 'hidden');
-	
+function listHideLowPriority(list, toggleItem) {
+	f.toggleClass(toggleItem, 'hidden');
+
 	setTimeout(function () {
-		f.toggleClass(f.select('.experience'), 'priorityLowVisible');
+		f.toggleClass(list, 'list--show-all');
 	}, 300);
 }
 
@@ -130,34 +131,37 @@ function listHideLowPriority() {
 
 // Document Load
 
-window.onload = function() {
-	
-// Show more / less
+window.onload = function () {
 
-	f.addEventListener(f.select('.toggleExperience'), 'click', function () {
-		
-		if (!f.hasClass(f.select('.experience'), 'priorityLowVisible')) {
-			
-			listShowLowPriority();
+	// Show more / less
+
+	f.addEventListener(f.select('.toggle-list-items'), 'click', function () {
+
+		var list = f.select('.list'),
+			toggleItem = f.select('.list__item--low-priority');
+
+		if (!f.hasClass(list, 'list--show-all')) {
+
+			listShowLowPriority(list, toggleItem);
 		} else {
-			
-			listHideLowPriority();
+
+			listHideLowPriority(list, toggleItem);
 		}
 	});
-	
-// Show more / less - END
-	
-	
-	
-// Width of Portfolio & References
-	
-	f.setStyle(f.select('.references .horizontalWrapper'), 'width', numberOfReferences * pageWidth * 50 + "%");
-	f.setStyle(f.select('.references article.page'), 'width', 100 / numberOfReferences + "%");
 
-// Width of Portfolio & References - END
-	
-	
-	
+	// Show more / less - END
+
+
+
+	// Width of Portfolio & References
+
+	f.setStyle(f.select('.references .horizontalWrapper'), 'width', settings.numberOfReferences * settings.pageWidth * 50 + '%');
+	f.setStyle(f.select('.references article.page'), 'width', 100 / settings.numberOfReferences + '%');
+
+	// Width of Portfolio & References - END
+
+
+
 }
 
 // Document Load - END
