968d1f8139f0c7ea32914ecd392b22cce302269a
Chris Christensen <>
2019-09-10 22:28:50 +0200

    Moved paths from gulpfile to package, added devBuild option to control minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,61 @@
 "use strict";
 
-// Idea: Sorting Scss properties...
+// TODOS
+// TODO: Sorting Scss properties.
+
+// INCLUDES
 
 const { src, dest, watch, series, parallel } = require("gulp");
-// Markup
+const browserSync = require("browser-sync").create();
+const del = require("del");
+const rename = require("gulp-rename");
+const gulpif = require("gulp-if");
 const htmlmin = require("gulp-htmlmin");
-// Styles
 const sass = require("gulp-sass");
 sass.compiler = require("node-sass");
 const autoprefixer = require("gulp-autoprefixer");
 const cleanCSS = require("gulp-clean-css");
-// Scripts
 const uglify = require("gulp-uglify");
-// Other
-const browserSync = require("browser-sync").create();
-const del = require("del");
-const rename = require("gulp-rename");
 
-const paths = {
-  generic: { src: "./src", dest: "./dist" },
-  markup: { src: "./src/*.html", dest: "./dist" },
-  styles: { src: "./src/sass/**/*.scss", dest: "./dist/css" },
-  scripts: { src: "./src/js/**/*.js", dest: "./dist/js" },
-  assets: { src: ["./src/font/*.*", "./src/img/**/*.*"], dest: "./dist" }
-};
+// Paths
+const { paths, devBuild } = require("./package.json");
+// Edit devBuild in package.json to control minifications and other optimizations
 
+// TASKS
+
+// Clean
 // Delete ./dist/ folder
 const clean = () => del(paths.generic.dest);
 exports.clean = clean;
 exports.clean.description = "Delete dist/ folder";
 
-// MARKUP
+// Markup
 // Minify markup and place in ./dist/
-// TODO: Make minify optional
 const markup = () =>
   src(paths.markup.src)
-    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulpif(!devBuild, htmlmin({ collapseWhitespace: true })))
     .pipe(dest(paths.markup.dest));
 
-// STYLES
-// Transpile Sass to CSS and minify and place in ./dist/css
-// TODO: Make use of prefixer (postCSS?)
+// Styles
+// Transpile Sass to CSS, minify and place in ./dist/css
 const styles = () =>
   src(paths.styles.src)
     .pipe(sass().on("error", sass.logError))
     .pipe(autoprefixer())
-    .pipe(dest(paths.styles.dest))
-    .pipe(cleanCSS())
-    .pipe(rename({ extname: ".min.css" }))
+    .pipe(gulpif(!devBuild, cleanCSS()))
+    .pipe(gulpif(!devBuild, rename({ extname: ".min.css" })))
     .pipe(dest(paths.styles.dest))
     .pipe(browserSync.stream());
 
-// SCRIPTS
+// Scripts
 // Minify scripts and place in ./dist/js
-// TODO: Make minify optional
 const scripts = () =>
   src(paths.scripts.src)
-    .pipe(dest(paths.scripts.dest))
-    .pipe(uglify())
-    .pipe(rename({ extname: ".min.js" }))
+    .pipe(gulpif(!devBuild, uglify()))
+    .pipe(gulpif(!devBuild, rename({ extname: ".min.js" })))
     .pipe(dest(paths.scripts.dest));
 
-// ASSETS
+// Assets
 // Copy assets to ./dist/
 const assets = () =>
   src(paths.assets.src, { base: paths.generic.src }).pipe(
@@ -69,20 +63,17 @@ const assets = () =>
   );
 
 // Build
-// TODO: Make build compress JS (and HTML)
-// TODO: Only build (not watch) should compress Sass
 const build = parallel(markup, styles, scripts, assets);
 exports.build = series(clean, build);
 exports.build.description = "Clean, build";
 
 // Watch
-// TODO: Make Watch not compress Sass and JS (and HTML)
 const watchFiles = done => {
   browserSync.init({ server: { baseDir: paths.generic.dest } });
-  watch(paths.markup.src, series(markup, browserSync.reload));
+  watch(paths.markup.src).on("change", series(markup, browserSync.reload));
   watch(paths.styles.src, styles);
-  watch(paths.scripts.src), series(scripts, browserSync.reload);
-  watch(paths.assets.src), series(assets, browserSync.reload);
+  watch(paths.scripts.src).on("change", series(scripts, browserSync.reload));
+  watch(paths.assets.src).on("change", series(assets, browserSync.reload));
   done();
 };
 watchFiles.displayName = "Watch";
