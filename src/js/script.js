bbdcb4e123b479f8f0e1b415a986f8a9c2639940
Chris Christensen <>
2019-09-29 22:40:51 +0200

    Added a message to nerds in the console.

--- src/js/script.js
+++ src/js/script.js
@@ -20,6 +20,21 @@ window.onload = function() {
   });
 
   // Show more / less - END
+
+  console.info(
+    '%cThis was built with a focus on performance. I know what you are thinking... go ahead... test it.' +
+      '\nDid I miss something? Tell me about it 🤙🏾',
+
+    'display: block;' +
+      'padding: 10px 18px;' +
+      'border: 1px solid rgba(0, 0, 0, 0.1);' +
+      'background-color: #f5f5f5;' +
+      'font-size: 14px;' +
+      'font-weight: 300;' +
+      'line-height: 26px;' +
+      'text-shadow: 1px 1px 0 rgba(0, 0, 0, 0.1);' +
+      'color: #424242;'
+  );
 };
 
 // Document Load - END
