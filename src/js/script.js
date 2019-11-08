21c27a772dc8dded21b88c16d719315f2be9f667
Chris Christensen <>
2019-11-08 21:43:51 +0100

    Simplified experience list, removed low-priority items.

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,23 @@
 'use strict';
 
-var f = require('./helper/functions');
-var list = require('./modules/list');
+// var f = require('./helper/functions');
+// var list = require('./modules/list');
 
 // Document Load
 
 window.onload = function() {
   // Show more / less
 
-  f.addEventListener(f.select('.toggle-list-items'), 'click', function() {
-    var listElement = f.select('.list'),
-      toggleItem = f.select('.list__item--low-priority');
+  // f.addEventListener(f.select('.toggle-list-items'), 'click', function() {
+  //   var listElement = f.select('.list'),
+  //     toggleItem = f.select('.list__item--low-priority');
 
-    if (!f.hasClass(listElement, 'list--show-all')) {
-      list.showLowPriority(listElement, toggleItem);
-    } else {
-      list.hideLowPriority(listElement, toggleItem);
-    }
-  });
+  //   if (!f.hasClass(listElement, 'list--show-all')) {
+  //     list.showLowPriority(listElement, toggleItem);
+  //   } else {
+  //     list.hideLowPriority(listElement, toggleItem);
+  //   }
+  // });
 
   // Show more / less - END
 
