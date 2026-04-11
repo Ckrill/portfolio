4c2c893a6eccce17f5b2b875115601bfa32f5c33
Chris Christensen <chchr@dfds.com>
2026-04-11 22:52:32 +0200

    chore: Upgrade dependencies. Add build visualizer.

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import { visualizer } from 'rollup-plugin-visualizer';
+
+export default defineConfig({
+  plugins: [
+    visualizer({
+      filename: 'stats.html',
+      open: true,
+    }),
+  ],
+});
